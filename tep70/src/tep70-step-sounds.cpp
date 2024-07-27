d2cd6c4f5a524b321ae4687a02e864d4e4222e81
maisvendoo <maisvendoo@yandex.ru>
2024-07-27 23:17:03 +0300

    Озвучены тумблеры и АЗВ пульта машиниста и помошника

--- tep70/src/tep70-step-sounds.cpp
+++ tep70/src/tep70-step-sounds.cpp
@@ -7,4 +7,17 @@ void TEP70::stepSoundsSignals(double t, double dt)
 {
     (void) t;
     (void) dt;
+
+    // Тумблеры и АЗВ на пульте машиниста
+    analogSignal[SOUND_TUMBLER_OP1] = tumbler_field_weak1.getSoundSignal(Trigger::CHANGE_SOUND);
+    analogSignal[SOUND_TUMBLER_OP2] = tumbler_field_weak2.getSoundSignal(Trigger::CHANGE_SOUND);
+    analogSignal[SOUND_TUMBLER_BLINDS_WATER] = tumbler_water_zaluzi.getSoundSignal(Trigger::CHANGE_SOUND);
+    analogSignal[SOUND_TUMBLER_BLINDS_OIL] = tumbler_oil_zaluzi.getSoundSignal(Trigger::CHANGE_SOUND);
+    analogSignal[SOUND_AZV_COMMON_CONTROL] = azv_common_control.getSoundSignal(Trigger::CHANGE_SOUND);
+    analogSignal[SOUND_AZV_LOCO_CONTROL] = azv_upr_tepl.getSoundSignal(Trigger::CHANGE_SOUND);
+    analogSignal[SOUND_AZV_FUIL_PUMP] = azv_fuel_pump.getSoundSignal(Trigger::CHANGE_SOUND);
+    analogSignal[SOUND_AZV_EPT] = azv_ept_on.getSoundSignal(Trigger::CHANGE_SOUND);
+    analogSignal[SOUND_AZV_EDT] = azv_edt_on.getSoundSignal(Trigger::CHANGE_SOUND);
+    analogSignal[SOUND_TUMBLER_VOLTAGE] = tumbler_voltage.getSoundSignal(Trigger::CHANGE_SOUND);
+    analogSignal[SOUND_TUMBLER_DISEL_STOP] = tumbler_disel_stop.getSoundSignal(Trigger::CHANGE_SOUND);
 }
