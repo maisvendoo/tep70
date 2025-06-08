27ec3f85a1f9f8c788a282a5dc1784662d68d517
maisvendoo <maisvendoo@yandex.ru>
2025-06-08 20:36:03 +0300

    Правки модели, текстур, кода

--- tep70/src/tep70-init-cabine-controls.cpp
+++ tep70/src/tep70-init-cabine-controls.cpp
@@ -13,18 +13,18 @@ void TEP70::initCabineControls(const QString &modules_dir, const QString &custom
     km->read_config("km-2202", custom_cfg_dir);
 
     tumbler_field_weak1.setNumPositions(3);
-    tumbler_field_weak1.setKeyCode(KEY_3);
+    tumbler_field_weak1.setKeyCode(KEY_4);
     tumbler_field_weak1.setState(1);
 
     tumbler_field_weak2.setNumPositions(3);
-    tumbler_field_weak2.setKeyCode(KEY_4);
+    tumbler_field_weak2.setKeyCode(KEY_5);
     tumbler_field_weak2.setState(1);
 
     tumbler_water_zaluzi.setNumPositions(3);
-    tumbler_water_zaluzi.setKeyCode(KEY_5);
+    tumbler_water_zaluzi.setKeyCode(KEY_6);
     tumbler_water_zaluzi.setState(1);
 
     tumbler_oil_zaluzi.setNumPositions(3);
-    tumbler_oil_zaluzi.setKeyCode(KEY_6);
+    tumbler_oil_zaluzi.setKeyCode(KEY_7);
     tumbler_oil_zaluzi.setState(1);
 }
